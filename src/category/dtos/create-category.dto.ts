c6342202fdbf98cde66b6006b03bfa8f5d83821b
T40Z <b.ratajczyk124@gmail.com>
2024-09-11 13:09:53 +0200

    Add validation pipes

--- src/category/dtos/create-category.dto.ts
+++ src/category/dtos/create-category.dto.ts
@@ -1,9 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   name: string;
 
+  @IsArray()
   @ApiProperty()
   expenseIds: string[];
 }
--- src/expense/dtos/create-expense.dto.ts
+++ src/expense/dtos/create-expense.dto.ts
@@ -1,18 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsArray,
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 
 export class CreateExpenseDto {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   title: string;
 
+  @IsString()
   @ApiProperty()
   description: string;
 
+  @IsNotEmpty()
+  @IsNumber()
   @ApiProperty()
   cost: number;
 
+  @IsNotEmpty()
+  @IsDateString()
   @ApiProperty()
   date: Date;
 
+  @IsArray()
   @ApiProperty()
   categoriesIds: string[];
 }
